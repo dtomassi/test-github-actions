8d583192f8900794f8c7f6eb8550312e8b4d5e4c
dtomassi <datomassi@ucdavis.edu>
2019-12-07 11:34:57 -0800

    Adding test.py

---
+++ tests.py
@@ -0,0 +1,11 @@
+from main import bfs
+
+class TestBFS:
+    def test_one(self):
+        assert(bfs([1,2,3,4,5], 1) == 1)
+
+    def test_two(self):
+        assert(bfs([1,2,3,4,5], 0) == -1)
+
+    def test_three(self):
+        assert(bfs([], 0) == -1)
